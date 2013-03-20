6d5c41feed4d128c11dc99862f2aef27391fc2ce
chandras10 <chandras10@yahoo.com>
2013-03-20 18:57:43 +0530

    Inventory Dashboard changes

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,41 +3,11 @@ class ReportsController < ApplicationController
   end
 
   def dash_inventory
-    @deviceinfos = Deviceinfo.all 
-
-    # DeviceName examples: Windows Phone, iPad, iPhone, etc;
-    @deviceNameCounters = Deviceinfo.find(:all,
-                              :select => 'devicename as label, count(*) as data',
-                              :group  => 'devicename')
-
-    @deviceNameLabels = @deviceNameCounters.map do |d|
-                                                  d.label
-                                                end
-
-    @deviceNameData = @deviceNameCounters.map do |d|
-                                                  d.data
-                                                end
-
-
-    @OsCounters = Deviceinfo.find(:all,
-                              :select => 'classname, count(*) as data', 
-                              :group  => 'classname')
-
-    @OsLabels = @OsCounters.map do |d|
-                                  d.classname
-                                end
-    @OsData = @OsCounters.map do |d|
-                                  d.data
-                                end
-
-    respond_to do | format |
-      format.html
-      format.json { render  :json => @deviceinfos }
-    end
+    @deviceinfos = Deviceinfo.all
   end
 
   def tbl_inventory
-    @deviceinfos = Deviceinfo.all 
+    @deviceinfos = Deviceinfo.all
   end
  
   def dash_bw
