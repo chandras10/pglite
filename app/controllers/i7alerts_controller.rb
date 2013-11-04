35298c18a234ffa83acedd6a83c29a3bbe0bd393
chandras10 <chandras10@yahoo.com>
2013-11-04 19:03:50 +0530

    Tooltip showing deviceinfo when hovering over a mac address

--- app/controllers/i7alerts_controller.rb
+++ app/controllers/i7alerts_controller.rb
@@ -8,14 +8,6 @@ class I7alertsController < ApplicationController
     dbQuery = addTimeLinesToDatabaseQuery(dbQuery)
     timeQueryString = dbQuery.to_sql.scan(/SELECT (.*) FROM .* WHERE\s+\((.*)\).*/i)
 
-
-    @alerts = I7alert.select('timestamp, i7alertdef.id as id, i7alertdef.priority as priority, 
-                              i7alertdef.description as description, i7alertclassdef.description as classtype, 
-                              proto, srcmac, srcip, srcport, dstmac, dstip, dstport, pcap, message').
-                      joins('LEFT OUTER JOIN i7alertdef ON i7alertdef.id = i7alert.id 
-                             LEFT OUTER JOIN i7alertclassdef ON i7alertclassdef.id = i7alertdef.classid').
-                      where("i7alertdef.classid NOT in (#{Rails.configuration.i7alerts_ignore_classes.join('')})").
-                      where("#{timeQueryString[0][1]}")
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: I7alertsDatatable.new(view_context, timeQueryString)}
--- app/datatables/i7alerts_datatable.rb
+++ app/datatables/i7alerts_datatable.rb
@@ -24,27 +24,22 @@ private
     #
     # Create tooltips for each "Source Mac" found in the alerts..
     #
-    devices = alerts.map {|a| a.srcmac}
+    devices = alerts.map {|a| a.srcmac}.uniq
     deviceHash = Hash.new
     devices.each do |d|
-      deviceinfo = Deviceinfo.where("macid = ?", d).first
-      deviceHash[d] = "<div class='CSSTableGenerator'><table>" +
-                      "<tr><td colspan=2> #{deviceinfo.username}</td></tr>" +
-                      "<tr><td>Device</td><td> #{deviceinfo.devicetype}</td></tr>" +
-                      "<tr><td>Operating System</td><td> #{deviceinfo.operatingsystem} #{deviceinfo.osversion}</td></tr>" +
-                      "<tr><td>DVI </td><td> #{deviceinfo.dvi}</td></tr>" +
-                      "<tr><td>DTI </td><td> #{deviceinfo.dti}</td></tr>" +
-                      "</table></div"
+      deviceHash[d] = Deviceinfo.where("macid = ?", d).first
     end
 
     alerts.map do |alert|
+      macIDTooltip = @view.render :partial=> 'layouts/tooltip_device', :formats=>[:html], :locals => {:device => deviceHash[alert.srcmac]}
+
       {
         timestamp: h(alert.timestamp.in_time_zone(timeZone).strftime("%Y-%m-%d %H:%M")),
         priority: h(alert.priority),
         classtype: h(alert.classtype),
         id: "<a href='#' title='" + alert.description + "'>#{alert.id}</a>",
         proto: h(alert.proto),
-        srcmac: link_to(alert.srcmac, {:action=> "device_details", :controller=> "reports", :device => alert.srcmac}, {:rel => "popover", :'data-content' => "#{deviceHash[alert.srcmac]}", :'data-original-title' => "#{alert.srcmac}" }),
+        srcmac: link_to(alert.srcmac, {:action=> "device_details", :controller=> "reports", :device => alert.srcmac}, {:rel => "popover", :'data-content' => "#{macIDTooltip}", :'data-original-title' => "#{alert.srcmac}" }),
         srcip: h(alert.srcip),
         srcport: h(alert.srcport),
         dstmac: h(alert.dstmac),
@@ -61,13 +56,13 @@ private
   end
 
   def fetch_alerts
-    alerts = I7alert.select('timestamp, i7alertdef.id as id, i7alertdef.priority as priority, 
+    alerts = I7alert.select('i7alertdef.id as id, timestamp, i7alertdef.priority as priority, 
                               i7alertdef.description as description, i7alertclassdef.description as classtype, 
                               proto, srcmac, srcip, srcport, dstmac, dstip, dstport, pcap, message').
                       joins('LEFT OUTER JOIN i7alertdef ON i7alertdef.id = i7alert.id 
                              LEFT OUTER JOIN i7alertclassdef ON i7alertclassdef.id = i7alertdef.classid').
                       where("i7alertdef.classid NOT in (#{Rails.configuration.i7alerts_ignore_classes.join('')})").
-                      where("#{@queryConditions[0][1]}")
+                      where("#{@queryConditions[0][1]}").scoped
     alerts = alerts.order("#{sort_column} #{sort_direction}")
     alerts = alerts.page(page).per_page(per_page)
     if params[:sSearch].present?
